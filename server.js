205e408b4e471766b482c266b5890f377f219067
PrMittal09 <prateekmittal075@gmail.com>
2019-11-24 00:37:53 +0530

    first app

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require('path');
 var bodyParser = require("body-parser");
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -7,15 +6,11 @@ var issue = require('./routes/issue');
 
 var app = express();
 app.use(bodyParser.json());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
